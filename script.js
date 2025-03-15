87e1eb33e6f297e3e4064abf4c79c7005015f30f
Rubén de Jesús Martínez Elhore y Reynoso <63992504+rubenelhore@users.noreply.github.com>
2025-03-15 17:35:03 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// script.js
+
 document.addEventListener('DOMContentLoaded', function() {
     // Hamburger - VERSIÓN SIMPLIFICADA
     const hamburgerBtn = document.querySelector('.hamburger-btn');
